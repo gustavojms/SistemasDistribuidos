29d3b5e81c1a609ca38f8c1a68b1a44e195142be
gustavojms <gustavo.joset0@hotmail.com>
2023-04-04 21:37:03 -0300

    Adding function to emit that a new user connected

--- chat/server.js
+++ chat/server.js
@@ -11,6 +11,7 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log("User connected")
+    io.emit('user message', 'User connected')
 
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg)
